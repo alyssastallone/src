22d122abd9abb97c82604507df3fd50a24bf3a88
Alyssa <alyssastallone@gmail.com>
2020-09-15 11:42:22 -0400

    9/15

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "sum.h"
+
+int main(void)
+{
+    print("Sum is %d\n,", sum(2,3));
+
+return 0;
+}
---
+++ sum.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_FILE
+    #define HEADER_FILE
+    int sum(int a, int b)
+#endif
